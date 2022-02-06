7a8fe1df2bf1962576d2fb88f3245980a711a8fa
Maria Gusova <masha.gu@gmail.com>
2022-02-06 17:16:54 +0000

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,9 @@
 const calculateB = (amount) => {
-  return String(Math.round(amount * 0.60));
+  return String(Math.round(amount * 0.597));
 };
 
 const calculateM = (amount) => {
-  return String(Math.round(amount * 0.40));
+  return String(Math.round(amount * 0.403));
 };
 
 export { calculateB, calculateM };
